28dfe10efdbfb901b908a142fc1faf9a59214cc5
mahadevTW <mahadev.wce@gmail.com>
2018-01-21 16:38:43 +0530

    first commit

---
+++ Data.java
@@ -0,0 +1,15 @@
+class Data
+{
+public static void main(String args[])
+{
+int a=10;
+
+
+float f=10.1f;
+double c=34.2;
+//System.out.println(f.getClass().getSimpleName());
+System.out.println("a "+a);
+System.out.println("f "+  f);
+System.out.println("c "+c);
+}
+}
---
+++ fizz.java
@@ -0,0 +1,15 @@
+class fizz
+{
+public static void main(String args[])
+{
+for( int i=0;i<=100;i++)
+{
+	if(i%3==0&&i%5==0)
+		System.out.println("fizzbuzz="+ i);
+	if(i%3==0)
+		System.out.println("fizz="+i);
+	if(i%5==0)
+		System.out.println("buzz="+i);
+}
+}
+}
